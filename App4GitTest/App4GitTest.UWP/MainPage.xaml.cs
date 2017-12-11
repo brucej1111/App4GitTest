a0e75d93195e7217e02b6f24cf16eb99420c63c7
Bruce <brucej1111@hotmail.com>
2017-12-11 12:32:22 -0600

    Add project files.

---
+++ App4GitTest/App4GitTest.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace App4GitTest.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new App4GitTest.App());
+        }
+    }
+}
